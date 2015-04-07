2cf9f7236174f81905380b895bc8442f2fd56877
McLeanBH <mcleanbh@gmail.com>
2015-04-07 01:20:04 -0400

    implementing responsive sliding background gallery-carousel

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+// $(function() {
+//     $('#section-eight').vegas({
+//         slides: [
+//             { src: '../img/brunch.jpg' },
+//             { src: '../img/computing.jpg' },
+//             { src: '../img/foggy-road.jpg' },
+//             { src: '../img/night-lights.jpg' },
+//             { src: '../img/programming.jpg' },
+//             { src: '../img/slopes.jpg' },
+//             { src: '../img/tunnel.jpg' },
+//             { src: '../img/window.jpg' }
+//         ]
+//     });
+// }
+
+$('#section-eight').vegas({
+    delay: 4000,
+    timer: false,
+    shuffle: true,
+    transition: 'fade',
+    transitionDuration: 2000,
+    slided: [
+        { src: '../img/brunch.jpg' },
+        { src: '../img/computing.jpg' },
+        { src: '../img/foggy-road.jpg' },
+        { src: '../img/night-lights.jpg' },
+        { src: '../img/programming.jpg' },
+        { src: '../img/slopes.jpg' },
+        { src: '../img/tunnel.jpg' },
+        { src: '../img/window.jpg' }
+    ]
+});
+
+
