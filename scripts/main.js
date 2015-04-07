e82fa8adf9c5306105777d6131644c6c770ceb94
McLeanBH <mcleanbh@gmail.com>
2015-04-07 08:39:51 -0400

    vegas photo gallery implemented and working

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,18 @@
-// $(function() {
-//     $('#section-eight').vegas({
-//         slides: [
-//             { src: '../img/brunch.jpg' },
-//             { src: '../img/computing.jpg' },
-//             { src: '../img/foggy-road.jpg' },
-//             { src: '../img/night-lights.jpg' },
-//             { src: '../img/programming.jpg' },
-//             { src: '../img/slopes.jpg' },
-//             { src: '../img/tunnel.jpg' },
-//             { src: '../img/window.jpg' }
-//         ]
-//     });
-// }
-
-$('#section-eight').vegas({
-    delay: 4000,
-    timer: false,
-    shuffle: true,
-    transition: 'fade',
-    transitionDuration: 2000,
-    slided: [
-        { src: '../img/brunch.jpg' },
-        { src: '../img/computing.jpg' },
-        { src: '../img/foggy-road.jpg' },
-        { src: '../img/night-lights.jpg' },
-        { src: '../img/programming.jpg' },
-        { src: '../img/slopes.jpg' },
-        { src: '../img/tunnel.jpg' },
-        { src: '../img/window.jpg' }
-    ]
+$('body').vegas({
+  overlay: true,
+  transition: 'fade', 
+  transitionDuration: 4000,
+  delay: 10000,
+  animation: 'random',
+  animationDuration: 20000,
+  slides: [
+    { src: 'img/brunch.jpg' },
+    { src: 'img/computing.jpg' },
+    { src: 'img/foggy-road.jpg' },
+    { src: 'img/night-lights.jpg' },
+    { src: 'img/programming.jpg' },
+    { src: 'img/slopes.jpg' },
+    { src: 'img/tunnel.jpg' },
+    { src: 'img/window.jpg' },
+  ]
 });
-
-
